c8f019a6de04275db0fd45c017d58134302020e9
Janaina <janainasoul@icloud.com>
2023-03-09 18:56:14 -0300

    aprendendo a criar metodos no js

--- index.js
+++ index.js
@@ -2,29 +2,50 @@ class Cliente { //molde do cliente
     nome;
     cpf;
     rg;
-    agencia;
-    saldo;
+    
 }
 
-const cliente1 = new Cliente (); //cada new éCleinte é um objeto - instaciando a classe 
-const cliente2 = new Cliente ();
+class ContaCorrente{
+    agencia;
+    #saldo = 0;
+
+    //encapsular dentro de um metodo
+    sacar(valor){ ///operação de saque do valor que deseja 
+        if(this.#saldo >= valor){ //this - dessa conta corrente
+            this.#saldo -= valor;
+        }
+    }     
+
+        depositar(valor){
+        if(valor >0){
+            this.#saldo += valor;
+            }
+        }
+}
 
 
-cliente1.nome = "Janaina"; //nome davariavel. atributo
+const cliente1 = new Cliente (); //cada new Cliente é um objeto - instaciando a classe 
+cliente1.nome = "Janaina"; //nome da variavel.atributo
 cliente1.rg = 341860815;
 cliente1.cpf= 11155533385;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-
-
+const cliente2 = new Cliente ();
 cliente2.nome = "Alice"; //nome davariavel. atributo
-
 cliente2.cpf= 12255533385;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteJanaina = new ContaCorrente();
+contaCorrenteJanaina.#saldo = 10000;
+contaCorrenteJanaina.agencia = 1001;
+
+contaCorrenteJanaina.depositar (100);
+contaCorrenteJanaina.sacar(50);
+
+console.log(contaCorrenteJanaina);
+
+
+
+// console.log(cliente1);
+// console.log(cliente2);
 //quais informações o ByteBank precisa?
 
 // const cliente1Nome = "Janaina";
